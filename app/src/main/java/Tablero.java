d28ae187adcb50bb7d7470db937367f8e9709556
Danichu15 <danielliszewski@hotmail.com>
2019-09-29 14:29:33 +0200

    Añadido método actualizar.

--- app/src/main/java/Tablero.java
+++ app/src/main/java/Tablero.java
@@ -4,4 +4,12 @@ public class Tablero {
     public Tablero(){
         this.matrizTablero = new int[9][23];
     }
+
+    //Este método tendrá que ser  llamado 2 veces, 1 para mover la pieza, y otra para borrar la pieza de su posición anterior, es decir pasándole un 0 como color
+    //y guardando las anteriores coordenas.
+    public void actualizarTablero(int [][] matrizCoords, int color){
+        for(int i = 0; i < matrizCoords.length; i++){
+            this.matrizTablero[matrizCoords[i][0]][matrizCoords[i][1]] = color;
+        }
+    }
 }
