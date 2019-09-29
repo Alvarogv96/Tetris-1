e6773a4ed2b22ed9d8824f86189234fee2022f4c
Danichu15 <danielliszewski@hotmail.com>
2019-09-29 14:08:29 +0200

    Se han añadido el método comprobar si la pieza ha llegado al fianl y el método que comprueba las filas que están completas.

--- app/src/main/java/Reglas.java
+++ app/src/main/java/Reglas.java
@@ -23,4 +23,29 @@ public class Reglas {
         return permiso;
     }
 
+    public boolean topeInferior(int [][] matrizCoords){
+        boolean tope = false;
+
+        for (int i = 0; i < matrizCoords.length; i++){
+            if(matrizCoords[i][1] > 23){
+                tope = true;
+            }
+        }
+        return tope;
+    }
+
+    public void filaCompleta (int [][] matrizTablero){
+        for (int i = matrizTablero.length; i > 0; i--){
+            boolean incompleta = false;
+            for(int j = 0; j < matrizTablero.length; j++){
+                if(matrizTablero[i][j] == 0){
+                    incompleta = true;
+                }
+            }
+            if(!incompleta){
+                //Juego.boorarFila(j);
+            }
+        }
+    }
+
 }
