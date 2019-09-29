070bdc04122157e5d55fc08da72af1f844d0b9ff
Danichu15 <danielliszewski@hotmail.com>
2019-09-29 13:46:30 +0200

    Se han creado los métodos que comprueban si una pieza se puede desplazar a la izquierda o derecha.

--- app/src/main/java/Reglas.java
+++ app/src/main/java/Reglas.java
@@ -1,2 +1,26 @@
 public class Reglas {
+    public boolean permisoDesplazamintoDer(int [][] matrizCoords){
+        boolean permiso = true;
+
+        for(int i = 0; i < matrizCoords.length; i++){
+            if (matrizCoords[i][0] > 10){
+                permiso = false;
+            }
+        }
+
+        return permiso;
+    }
+
+    public boolean permisoDesplazamintoIzq(int [][] matrizCoords){
+        boolean permiso = true;
+
+        for(int i = 0; i < matrizCoords.length; i++){
+            if (matrizCoords[i][0] < 0){
+                permiso = false;
+            }
+        }
+
+        return permiso;
+    }
+
 }
