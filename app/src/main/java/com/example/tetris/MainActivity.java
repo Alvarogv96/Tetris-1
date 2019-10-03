cdc61c1bbc1a768ea18df51fe4aa0669a6211cc4
Wilhelm-mar <45694504+Wilhelm-mar@users.noreply.github.com>
2019-10-03 21:26:19 +0200

    Update MainActivity.java
    Agregar metodos de control para l segundo plano.

--- app/src/main/java/com/example/tetris/MainActivity.java
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // La actividad se crea.
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // La actividad está iniciando.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Aplicacíon iniciada", Toast.LENGTH_SHORT).show();
+        // La actividad se hace visible.
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //La actividad se está pausando.
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Pausando", Toast.LENGTH_SHORT).show();
+        // La actividad se detiene.
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // La actividad se destruye.
+    }
+
 }
