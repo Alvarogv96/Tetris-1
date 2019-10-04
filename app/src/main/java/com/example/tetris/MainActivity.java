f7643e9882ed7dbb4aea7e07e8f7a1f3c3e53e78
Avadem <alex.gavi@hotmail.com>
2019-10-04 17:15:36 +0200

    Imagenes añadidas

---
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tetris;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+
+    }
+    public static void main(String [] args)
+    {
+
+    }
+
+}
